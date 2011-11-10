b5c05c5e9bb115622b77218536f43d67d6b2aa8f
Lars Fronius <lars@jimdo.com>
2011-11-10 15:42:28 +0100

    Added configfile function to site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,3 +11,16 @@ Exec {
 exec { "apt-get-update" :
     command => "/usr/bin/apt-get update",
 }
+
+define configfile($owner = root, $group = root, $mode = 0644, $ensure = file, $links = manage, $source, $recurse = false, $path = false) {
+  file { $name :
+    ensure => $ensure,
+    links => $links,
+    owner => $owner,
+    group => $group,
+    mode => $mode,
+    recurse => $recurse,
+    path => $path ? { false => $name, default => $path },
+    source => $source,
+  }
+}
